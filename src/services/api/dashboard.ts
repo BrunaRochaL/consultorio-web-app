de97ea1ada66741c37a0fdb8691ad1b6a62e622a
Bruna Lourenco <bruna.guex@faculdadecesusc.edu.br>
2024-12-16 05:13:16 -0300

    Set type direct in class

--- src/services/api/dashboard.ts
+++ src/services/api/dashboard.ts
@@ -1,6 +1,29 @@
 import { Appointment } from './appointments'
 import { baseApi } from './baseApi'
-import { DayData } from '@/types/dashboard'
+
+export interface Patient {
+  id: number
+  name: string
+  avatar: string
+  appointmentTime: string
+  isCompleted: boolean
+}
+
+export interface DailyStats {
+  patients: number
+  appointments: number
+  revenue: number
+  compareToPrevious: {
+    patients: number
+    appointments: number
+    revenue: number
+  }
+}
+
+export interface DayData {
+  stats: DailyStats
+  patients: Patient[]
+}
 
 export const dashboardApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
