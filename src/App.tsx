9facaf166c2a3bdcd73684a73e5237444670fe34
Bruna Lourenco <bruna.guex@faculdadecesusc.edu.br>
2024-12-16 02:03:39 -0300

    Add toast notifications and masks

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 import './App.css'
 import Layout from './components/common/Layout/Layout'
@@ -9,6 +11,18 @@ function App() {
     <BrowserRouter>
       <Layout>
         <AppRoutes />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
       </Layout>
     </BrowserRouter>
   )
