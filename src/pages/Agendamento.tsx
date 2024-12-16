87bad044e4d39ce34a06b7284aa0c20559964a9e
Bruna Lourenco <bruna.guex@faculdadecesusc.edu.br>
2024-12-15 21:53:36 -0300

    Add scheduling page and remove old non-existent ones

---
+++ src/pages/Agendamento.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const Agendamento: React.FC = () => {
+  return (
+    <Container>
+      <h1>Agendar Consulta</h1>
+    </Container>
+  )
+}
+
+export default Agendamento
